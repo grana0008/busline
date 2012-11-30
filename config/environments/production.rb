644bf6d80430a47d0a946a61ed9208561c27f4fb
Zack Siri <zack@artellectual.com>
2012-11-30 14:09:37 +0700

    add jquery first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Busline::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( active_admin.js landmarks.js busstops.js home.js gmaps4rails.css jquery.js )
+  config.assets.precompile += %w( jquery.js active_admin.js landmarks.js busstops.js home.js gmaps4rails.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
